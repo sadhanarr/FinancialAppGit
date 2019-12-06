52dbbcef60f507a14d1e7a7ffa7e96131199659e
unknown <sadhu.malu@gmail.com>
2019-12-06 05:39:19 +0530

    bug fixing

--- src/app/home-loan/home-loan.component.ts
+++ src/app/home-loan/home-loan.component.ts
@@ -117,6 +117,7 @@ loanStat:LoanStatus= new LoanStatus(0,'','','','','','',0);
     this.EGuaranProof.push({ID:this.CustID,CustType:'Guarantor',ProofType:this.proof,ProofNumber:this.proofNumber,File:this.selectedFile})
     this.proof=''; this.proofNumber=''; $('#file').val('')
   }
+ 
   }
   OpenFile(url:string)
   {
@@ -161,11 +162,12 @@ loanStat:LoanStatus= new LoanStatus(0,'','','','','','',0);
    
   }
 
-  filterdrop()
+  filterdrop(value)
   {
    
-    if(this.loandetail["SubAreaID"]== undefined)
+    if(this.loandetail["SubAreaID"]== undefined || value=='')
     {
+      console.log("Anu")
     if(this.loandetail["BranchID"]!= undefined)
     {
      this.Line= this.AllLine.filter(x=> x.DistrictID== this.loandetail["BranchID"])
@@ -189,6 +191,8 @@ loanStat:LoanStatus= new LoanStatus(0,'','','','','','',0);
       this.Line=this.AllLine
       console.log(this.Area)
       this.loandetail.LineID= this.Area.filter(x=>x.AreaID== this.loandetail.AreaID)[0].LineID;
+     // this.loandetail.LineID=1010
+      console.log(this.loandetail.LineID)
       this.branch= this.Allbranch;
       this.loandetail.BranchID=this.Line.filter(x=>x.TalukID==this.loandetail.LineID)[0].DistrictID
       }
@@ -342,6 +346,104 @@ loanStat:LoanStatus= new LoanStatus(0,'','','','','','',0);
     })
 
   }
+  print(value:Collection)
+  {
+    var string=`<html>
+    <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css'>
+    <style>
+    .logo
+    {
+        width: 80px;
+        padding: 5px;
+    }
+    label,.algn
+    {
+    text-align:right !important;
+    }
+    </style>
+    <script>
+    function myFunction() {
+      document.getElementById('btn').style.visibility = 'hidden';
+      window.print();
+     
+    }
+    </script>
+    </head>
+    <body>
+           <div class='row'>
+    
+                        <div class=' form-horizontal col-md-6'>
+              <div class='form-group' style='margin-left:20px'>
+                         <img class='col-sm-2 algn' src='assets/Logo.jpg' style='height:10%;'>
+                        <label class='control-label col-sm-5' style='text-align:left !important'>Sri Navaladi Marudham Capitals,Cheyyar</label>
+            
+                          </div>
+                <div class='form-group'>
+                 
+                          
+                 </div>
+                            <div class='form-group'>
+                                <label class='control-label col-sm-3' style='text-align:right !important; width:250px!important'>Receipt No :</label>`
+                  +value.BillBook+
+                  ` </div>
+                             
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Date/Time :</label>`
+                  + new Date() +`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Line/Area :</label>
+                   `+ this.AllLine.filter(x=> x.TalukID== this.loandetail.LineID)[0].TalukName+`/`+this.AllArea.filter(x=>x.AreaID== this.loandetail.AreaID)[0].AreaName+
+                  ` </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Customer ID :</label>
+                   `+ this.loandetail.CustomerID+`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='text-align:right !important; width:250px!important'>Customer Name :</label>
+                  `+this.loandetail.CustName+`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Loan Category :</label>
+                   `+ this.LoanCategory.filter(x=>x.dropdownKey== this.loandetail.LoanCatID)[0].dropdownName +`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Loan No :</label>
+                   `+ this.loandetail.LoanNo+`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Due Receipt :</label>
+                  `+value.Duereciept+`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Penalty :</label>
+                                `+value.Penaltyreciept+`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='text-align:right !important; width:250px!important'>Net Received :</label>
+                   `+value.Duereciept+value.Penaltyreciept +`
+                   </div>
+                    <div class='form-group'>
+   
+                    <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Due Balance :</label>
+                    `+value.dueBal +`
+                   </div>
+                    <div class='form-group'>
+                                <label class='control-label col-sm-3' style='font-weight: 100;text-align:right !important; width:250px!important'>Loan Balance :</label>
+                                `+value.TotalBal +`
+                   </div>
+                   </div>
+                   </div>
+
+    </body>
+    </html>`
+    var wnd = window.open("", "_blank");
+wnd.document.write(string);
+wnd.print();
+  }
+
   DateChange()
   {
    
@@ -425,30 +527,12 @@ EmiDisable()
  OnChange()
   {
 this.EmiDisable();
-console.log("calleed");    
-    if(this.loandetail["LoanID"] ==0){
-    if(this.loandetail["LoanCatID"]==1)
-      this.loandetail["LoanNo"]= 100000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==2)
-      this.loandetail["LoanNo"]= 200000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==3)
-      this.loandetail["LoanNo"]= 300000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==4)
-      this.loandetail["LoanNo"]= 400000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==5)
-      this.loandetail["LoanNo"]= 500000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==6)
-      this.loandetail["LoanNo"]= 600000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==7)
-      this.loandetail["LoanNo"]= 700000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-      if(this.loandetail["LoanCatID"]==8)
-      this.loandetail["LoanNo"]= 800000+1+  this.loan.filter(x=>x.LoanCatID==this.loandetail["LoanCatID"]).length
-    }
+
    if(this.loandetail["LoanCatID"] !=null)
    {
      this.Sagent=this.AllSagent.filter(x=>x.LoanCategory==this.loandetail["LoanCatID"].toString())
    }
-   
+   this.loandetail.TotLoanAmt= this.loandetail.LoanAmt
    this._appService.getDueDate(new Date(this.loandetail.LoanDate).toLocaleDateString().replace('/','-').replace('/','-'),this.loandetail.DueType,this.loandetail.Instalments).subscribe(res=>
     {
       this.due= res;
@@ -490,11 +574,14 @@ console.log("calleed");
     var scheme=0;
     var DocCharge=0;
     if(this.loandetail["DueType"]=="EMI"){
-    this.loandetail["TotLoanAmt"]= this.loandetail["DocCharge"] != null? parseInt(this.loandetail["DocCharge"].toString())+parseInt(this.loandetail["LoanAmt"].toString()):parseInt(this.loandetail["LoanAmt"].toString());
+   // this.loandetail["TotLoanAmt"]= this.loandetail["DocCharge"] != null? parseInt(this.loandetail["DocCharge"].toString())+parseInt(this.loandetail["LoanAmt"].toString()):parseInt(this.loandetail["LoanAmt"].toString());
     this.loandetail["InterestAmt"]= Math.round( (parseInt(this.loandetail["TotLoanAmt"].toString())* parseInt(this.loandetail["Instalments"].toString())* parseFloat(this.loandetail["IntRate"].toString()) )/100)
     this.loandetail["TotAmount"]= parseInt( this.loandetail["TotLoanAmt"].toString())  +  parseInt(this.loandetail["InterestAmt"].toString())
     this.loandetail["InstalmentAmt"]= parseInt( this.loandetail["TotAmount"].toString())/ parseInt(this.loandetail["Instalments"].toString())
- 
+    if(this.loandetail["DocCharge"]!= null && this.loandetail["DocCharge"].toString()!= "" && this.loandetail["DocChargeRecvd"]!=null && this.loandetail["DocChargeRecvd"].toString() =="Add with Loan Amount" && this.loandetail["DocCharge"].toString() !="")
+    {
+      this.loandetail["TotLoanAmt"]= this.loandetail["DocCharge"] != null? parseInt(this.loandetail["DocCharge"].toString())+parseInt(this.loandetail["LoanAmt"].toString()):parseInt(this.loandetail["LoanAmt"].toString());
+    }
  if(this.loandetail["RoundedOff"])
  {
   this.loandetail["InstalmentAmt"]=Math.ceil(parseFloat(this.loandetail["InstalmentAmt"].toString())/5)*5;
@@ -529,6 +616,7 @@ console.log("calleed");
    {
      DocCharge=parseInt(this.loandetail["DocCharge"].toString())
    }
+
    this.loandetail["NetAmt"]= parseInt(this.loandetail["LoanAmt"].toString())+incentive-DocCharge-scheme;
   }
   if(this.loandetail["DueType"]=="Interest"){
